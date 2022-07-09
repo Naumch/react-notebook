229e11a198d565dd87e4febf1b32373c3c158d0e
Anna <anya.naumova.1994@gmail.com>
2022-07-09 13:52:24 +0300

    Объединила компоненты Notes и Note

--- my-app/src/Notes.js
+++ my-app/src/Notes.js
@@ -1,15 +1,22 @@
-import Note from './Note';
 
 function Notes({ notes, setNotes, filteredNotes, setEditId }) {
 
   const items = filteredNotes.map(note => {
-    return <Note 
-      key={note.id}
-      id={note.id}
-      title={note.title}
-      setEditId={setEditId}
-      remItem={remItem}
-    />
+    return (
+      <li 
+        className='list-item' 
+        key={note.id} 
+        onClick={() => setEditId(note.id)}
+      >
+        {note.title}
+        <button 
+          className="button-delete" 
+          onClick={() => remItem(note.id)}
+        >
+          ✕
+        </button>
+      </li>
+    )
   });
 
   function remItem(id) {
